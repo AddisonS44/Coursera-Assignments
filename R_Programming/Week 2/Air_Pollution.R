beacc9dba81ed8b241b35380ce90f7d805f64972
AddisonS44 <AddisonTSmith44@gmail.com>
2022-07-28 21:47:05 -0500

    Completed part 1 of R Programming Week 2

---
+++ R_Programming/Week 2/Air_Pollution.R
@@ -0,0 +1,25 @@
+## Write a function called "pollutantmean" which will average the pollutant chosen across all measurements for all monitors in a given range.
+
+## Inputs are the filepath where the data is stored, the pollutant (either sulfate or nitrate), and an integer vector of monitors
+## Return the value of all non-NA values
+
+pollutantmean <- function(Directory, pollutant, id) {
+  collected_csv <- NULL
+  for(i in id){
+    
+    if(i < 10){
+      datapath <- paste(Directory, "\\00",as.character(i), ".csv", sep= '')
+    } else if(i < 100) {
+      datapath <- paste(Directory, "\\0",as.character(i), ".csv", sep = '')
+    } else {
+      datapath<- paste(Directory, "\\", as.character(i), ".csv", sep = '')
+    }
+    
+    current_csv <- read.csv(datapath)
+    current_cleaned_csv <- current_csv[!is.na(current_csv[,pollutant]),]
+    
+    collected_csv <- rbind(collected_csv, current_cleaned_csv)
+
+  }
+  mean(collected_csv[,pollutant])
+}
